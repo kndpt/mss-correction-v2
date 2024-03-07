42f8a5523a129f687eab53800cccad2db5b00383
kndpt <dupont.kelvin@gmail.com>
2024-03-07 07:44:27 +0100

    fix(seo): improve seo

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -6,10 +6,8 @@ import { PostListHomeView } from 'src/sections/blog/view';
 
 export const metadata = {
   title: 'Blog - Mss Correction',
-  // TODO: Add description
-  description: '',
+  description: `Découvrez mes astuces pour améliorer votre écriture, rédiger un CV, une lettre de motivation, ou encore un roman.`,
 };
-
 export default function PostListHomePage() {
   return (
     <Box sx={{ pt: { xs: 12, md: 16 } }}>
