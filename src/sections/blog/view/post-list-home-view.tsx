f85e27f702bbbebaf4868483e1e604803c6ab64b
kndpt <dupont.kelvin@gmail.com>
2024-03-07 08:22:03 +0100

    fix(seo): improve seo interne

--- src/sections/blog/view/post-list-home-view.tsx
+++ src/sections/blog/view/post-list-home-view.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Stack } from '@mui/material';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
@@ -24,14 +25,38 @@ export default function PostListHomeView() {
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'}>
-      <Typography
-        variant="h4"
+      <Stack
+        spacing={2}
         sx={{
-          my: { xs: 3, md: 5 },
+          textAlign: 'center',
+          mt: 4,
+          mb: 8,
+          mx: {
+            xs: 1,
+            sm: 3,
+            md: 5,
+            lg: 10,
+            xl: 20,
+          },
         }}
       >
-        Blog
-      </Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            my: { xs: 3, md: 5 },
+            fontSize: '36px!important',
+          }}
+        >
+          Blog
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ color: 'text.secondary', fontWeight: 'normal', fontSize: '14px!important' }}
+        >
+          Découvrez mes conseils pour améliorer votre écriture, rédiger un CV, une lettre de
+          motivation, ou encore un roman.
+        </Typography>
+      </Stack>
       <PostList posts={posts} loading={postLoading} />
     </Container>
   );
