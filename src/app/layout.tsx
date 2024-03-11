2052e685c22c246cf67a8a6a96ef94dd98673c57
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-03-11 18:30:26 +0100

    feat(counter): add counter analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,6 +110,16 @@ export default function RootLayout({ children }: Props) {
     />
   );
 
+  const buildCounterAnalytics = () => (
+    <Script
+      id="counter-dev-script"
+      src="https://cdn.counter.dev/script.js"
+      data-id="7a999196-c909-4480-9af6-f7d08bc00fc4"
+      data-utcoffset="1"
+      strategy="afterInteractive"
+    />
+  );
+  
   return (
     <html lang="fr" className={primaryFont.className}>
       <body>
@@ -145,7 +155,8 @@ export default function RootLayout({ children }: Props) {
         />
         {isEnvironment(EENV.PRODUCTION) && <SpeedInsights />}
         {isEnvironment(EENV.PRODUCTION) && buildHotjar()}
-        {isEnvironment(EENV.PRODUCTION) && buildGoogleAnalytics()}
+        {isEnvironment(EENV.PRODUCTION) && buildCounterAnalytics()}
+        {/* {isEnvironment(EENV.PRODUCTION) && buildGoogleAnalytics()} */}
         {isEnvironment(EENV.PRODUCTION) && buildChatbot()}
       </body>
     </html>
