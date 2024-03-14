da67ce44473b6c12dadd761541fd16d9038f6f0d
kndpt <dupont.kelvin@gmail.com>
2024-03-14 14:22:50 +0100

    Revert "test(home): remove framer motion on document section"
    This reverts commit b9f92cfacc7c60490aa5298f23ddfa9960dd7da8.

--- src/sections/home/home-documents.tsx
+++ src/sections/home/home-documents.tsx
@@ -1,3 +1,5 @@
+import { m } from 'framer-motion';
+
 import Box from '@mui/material/Box';
 import { Link } from '@mui/material';
 import Card from '@mui/material/Card';
@@ -6,7 +8,7 @@ import { alpha } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-import { MotionViewport } from 'src/components/animate';
+import { varFade, MotionViewport } from 'src/components/animate';
 
 // ----------------------------------------------------------------------
 
@@ -55,19 +57,25 @@ export default function HomeDocuments() {
           mb: { xs: 5, md: 10 },
         }}
       >
-        <Typography component="div" variant="overline" sx={{ color: 'text.disabled' }}>
-          Correction
-        </Typography>
+        <m.div variants={varFade().inUp}>
+          <Typography component="div" variant="overline" sx={{ color: 'text.disabled' }}>
+            Correction
+          </Typography>
+        </m.div>
 
-        <Typography variant="h2">
-          Tous types <br /> de documents
-        </Typography>
+        <m.div variants={varFade().inDown}>
+          <Typography variant="h2">
+            Tous types <br /> de documents
+          </Typography>
+        </m.div>
 
-        <Typography sx={{ color: 'grey.500' }}>
-          Mon service de correction s&apos;adapte aux spécificités de chaque type de document,
-          respectant votre style personnel tout en appliquant les corrections nécessaires pour un
-          texte professionnel.
-        </Typography>
+        <m.div variants={varFade().inUp}>
+          <Typography sx={{ color: 'grey.500' }}>
+            Mon service de correction s&apos;adapte aux spécificités de chaque type de document,
+            respectant votre style personnel tout en appliquant les corrections nécessaires pour un
+            texte professionnel.
+          </Typography>
+        </m.div>
       </Stack>
 
       <Box
@@ -80,38 +88,40 @@ export default function HomeDocuments() {
         }}
       >
         {CARDS.map((card, index) => (
-          <Card
-            sx={{
-              textAlign: 'center',
-              boxShadow: { md: 'none' },
-              bgcolor: 'background.default',
-              p: (theme) => theme.spacing(10, 5),
-              ...(index === 1 && {
-                boxShadow: (theme) => ({
-                  md: `-40px 40px 80px ${
-                    theme.palette.mode === 'light'
-                      ? alpha(theme.palette.grey[500], 0.16)
-                      : alpha(theme.palette.common.black, 0.4)
-                  }`,
+          <m.div variants={varFade().inUp} key={card.title}>
+            <Card
+              sx={{
+                textAlign: 'center',
+                boxShadow: { md: 'none' },
+                bgcolor: 'background.default',
+                p: (theme) => theme.spacing(10, 5),
+                ...(index === 1 && {
+                  boxShadow: (theme) => ({
+                    md: `-40px 40px 80px ${
+                      theme.palette.mode === 'light'
+                        ? alpha(theme.palette.grey[500], 0.16)
+                        : alpha(theme.palette.common.black, 0.4)
+                    }`,
+                  }),
                 }),
-              }),
-            }}
-          >
-            <Box
-              component="img"
-              src={card.icon}
-              alt={card.title}
-              sx={{ mx: 'auto', width: 70, height: 70 }}
-            />
+              }}
+            >
+              <Box
+                component="img"
+                src={card.icon}
+                alt={card.title}
+                sx={{ mx: 'auto', width: 70, height: 70 }}
+              />
 
-            <Link href={card.href} sx={{ textDecoration: 'none', color: 'inherit' }}>
-              <Typography variant="h5" sx={{ mt: 8, mb: 2 }}>
-                {card.title}
-              </Typography>
-            </Link>
+              <Link href={card.href} sx={{ textDecoration: 'none', color: 'inherit' }}>
+                <Typography variant="h5" sx={{ mt: 8, mb: 2 }}>
+                  {card.title}
+                </Typography>
+              </Link>
 
-            <Typography sx={{ color: 'text.secondary' }}>{card.description}</Typography>
-          </Card>
+              <Typography sx={{ color: 'text.secondary' }}>{card.description}</Typography>
+            </Card>
+          </m.div>
         ))}
       </Box>
     </Container>
