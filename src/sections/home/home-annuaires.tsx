64628978efd359232958ec094c31047d89504b9c
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-04-06 18:00:54 +0200

    develop (#14)
    * Update README.md
    
    * fix(schema): offer lettre motivation
    
    * feat(annuaires): add annuaire section on home page
    
    * add intellij config
    
    * feat(annuaires): add la denis in annuaire section

--- src/sections/home/home-annuaires.tsx
+++ src/sections/home/home-annuaires.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
-
-import Link  from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
+import Link      from '@mui/material/Link';
+import { Box }            from '@mui/system';
 import Container from '@mui/material/Container';
-
-import { MotionViewport } from '../../components/animate';
 
 // ----------------------------------------------------------------------
 
 export default function HomeAnnuaires() {
   return (
-    <Container component={MotionViewport} sx={{ pb: 8 }}>
-      <Stack justifyContent="center">
+    <Container sx={{ pb: 8, display: 'flex', justifyContent: 'center' }}>
+      <Box sx={{ width: 75 }}>
         <Link href="https://www.gralon.net/annuaire/commerce-et-societe/services/secretariat.htm" target="_blank">
           <Image src="https://logo.gralon.net/plogo2-gralon-204956.gif" alt="Services" width={75} height={30} />
         </Link>
-      </Stack>
+      </Box>
+      <Box sx={{ width: 75, ml: 2 }}>
+        <Link href="https://www.ladenise.com" target="_blank">
+          <Image src="https://www.ladenise.com/wp-content/uploads/logo-327-ladenise-transparent.png" alt="Services"
+                 width={75} height={50} />
+        </Link>
+      </Box>
     </Container>
   );
 }
