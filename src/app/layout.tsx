7ac8920f5f40de4201fcfad0a083b505191b2421
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-08-04 20:06:56 +0200

    fix: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,6 +118,38 @@ export default function RootLayout({ children }: Props) {
     />
   );
 
+  const buildTagManager = () => (
+    <>
+      {/* Google Tag Manager */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T4NMPXX9');
+          `,
+        }}
+      />
+      {/* End Google Tag Manager */}
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          title="google-tag-manager"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-T4NMPXX9"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+    </>
+  );
+
   const buildCounterAnalytics = () => (
     <Script
       id="counter-dev-script"
@@ -130,6 +162,7 @@ export default function RootLayout({ children }: Props) {
 
   return (
     <html lang="fr" className={primaryFont.className}>
+      <head>{isEnvironment(EENV.PRODUCTION) && buildTagManager()}</head>
       <body>
         <AuthProvider>
           <LocalizationProvider>
