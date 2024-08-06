c6814c259ef64f380a7c25b1d50446d8a13c6302
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-08-06 16:13:59 +0200

    feat: add some events in simple analytics

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { sendSimpleAnalyticsEvent } from 'src/utils/utils';
+
 import ServiceView from 'src/sections/service/service-view';
 
+import { ESimpleAnalyticsEvent } from 'src/types/simple-analytics-event';
+
 export default function ServicePage() {
+  useEffect(() => {
+    sendSimpleAnalyticsEvent(ESimpleAnalyticsEvent.PAGE_VIEWED_SERVICE);
+  }, []);
+
   return <ServiceView />;
 }
--- src/types/simple-analytics-event.ts
+++ src/types/simple-analytics-event.ts
@@ -1,3 +1,5 @@
 export enum ESimpleAnalyticsEvent {
   USER_REGISTERED = 'user_registered',
+  PAGE_VIEWED_SERVICE = 'page_viewed_service',
+  PAGE_VIEWED_SUCCESS_ORDER = 'page_viewed_success_order',
 }
