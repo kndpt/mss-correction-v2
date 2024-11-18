3546b86f044eb9d7efce5de46e8389071682d7db
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-11-18 12:14:04 +0100

    fix: add livre keyword in roman page

--- src/app/correction-roman/layout.tsx
+++ src/app/correction-roman/layout.tsx
@@ -11,8 +11,8 @@ import MainLayout from 'src/layouts/main';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Service de relecture et correction de roman à petit prix - Mss Correction',
-  description: `Professionnelle en correction de roman, j'offre une relecture précise et un service d'embellissement de texte. Tarifs abordables, simulateur en ligne. Améliorez votre manuscrit avec une experte.`,
+  title: 'Service de relecture correction de roman et autres livres à petit prix',
+  description: `Professionnelle en correction de roman, j'offre une relecture précise et un service d'embellissement de texte. Tarifs abordables, simulateur en ligne. Améliorez votre livre avec une experte.`,
 };
 
 type Props = {
