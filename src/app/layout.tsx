f84a1dedad06c5e27fa35d65c9dd1da0d0e98895
Kelvin Dupont <116877495+kndpt@users.noreply.github.com>
2024-12-16 22:56:38 +0100

    fix: try open widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,22 @@ export default function RootLayout({ children }: Props) {
     />
   );
 
+  const buildOpenWidget = () => (
+    <Script
+      id="openwidget-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.__ow = window.__ow || {};
+          window.__ow.organizationId = "6a0a9d0e-47d3-4619-a9b7-1aeb064283df";
+          window.__ow.integration_name = "manual_settings";
+          window.__ow.product_name = "openwidget";   
+          ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))
+        `,
+      }}
+    />
+  );
+
   return (
     <html lang="fr" className={primaryFont.className}>
       <head>
@@ -153,7 +169,8 @@ export default function RootLayout({ children }: Props) {
         {isEnvironment(EENV.PRODUCTION) && <Analytics />}
         {isEnvironment(EENV.PRODUCTION) && <SpeedInsights />}
         {isEnvironment(EENV.PRODUCTION) && buildHotjar()}
-        {isEnvironment(EENV.PRODUCTION) && buildChatbot()}
+        {/* {isEnvironment(EENV.PRODUCTION) && buildChatbot()} */}
+        {isEnvironment(EENV.PRODUCTION) && buildOpenWidget()}
       </body>
     </html>
   );
